a9cc9f4b757c2351363a395d8f3a605414994bf8
Tarek BACHIR <bachir.tarek.pro@gmail.com>
2025-07-19 01:01:21 +0200

    Deploy: production build

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+// tsup.config.js
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  minify: true,
+  external: ['jszip']
+})
